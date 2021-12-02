1a707e476257fa8a9d6d84a5ac657af3d4784d02
Kurt <kurtmilla1994@gmail.com>
2021-12-01 23:34:13 -0500

    made mock db using JSON

--- p1.py
+++ p1.py
@@ -1,18 +1,23 @@
-myDict = {}
+import json
+from func import *
+
 createUser = input("Press 1 to Add a User, 2 to Remove a User, or 3 to view list of Users: ")
 
 while createUser == "1":
-		f = open('dict.txt', 'w')
 		name = input("What is your name: ")
 		age = input("What is your age: ")
 
-		myDict[name]=age
-		f.write( str(myDict) )
+		data = {
+			"name": name,
+			"age": age
+		}
+
+		write_json(data)
+
 		add = input("Would you like to modify your data? Press 1 to add, 3 to list users, or 2 to close: ")	
 		
 		if add == "2":
-			f.close()
 			break
 
 		if add == '3':
-			print(myDict)
+			break
