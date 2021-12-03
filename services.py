352fceca70d656f6d4c5c7c56233082e1cc8951e
Matt <44270893+thesbx@users.noreply.github.com>
2021-12-03 10:30:16 -0500

    Update services.py

--- services.py
+++ services.py
@@ -1,35 +1,35 @@
 import json
-from api import *
-from services import *
+import api
 import uuid
 
-def addUser():
-	name = input("enter your name: ")
-	age = input("enter your age: ")
-	phone =	input("enter your phone number: ")
-	email =	input("enter your phone email: ")
+class Services:
+	def addUser():
+		name = input("enter your name: ")
+		age = input("enter your age: ")
+		phone =	input("enter your phone number: ")
+		email =	input("enter your phone email: ")
 
-	data = {
-		"person": {
-			"uuid": str(uuid.uuid4()),
-			"personal_info": {
-				"name": name,
-				"age": age
-			},
-			"contact_info": {
-				"phone": phone,
-				"email": email
+		data = {
+			"person": {
+				"uuid": str(uuid.uuid4()),
+				"personal_info": {
+					"name": name,
+					"age": age
+				},
+				"contact_info": {
+					"phone": phone,
+					"email": email
+				}
 			}
 		}
-	}
-	update_json(data)
+		update_json(data)
 
-def inputData(x):
-    if x == "1" or x == 'yes' or x == 'y':
-        addUser()
-    elif x == "2" or x == 'no' or x == 'n':
-        exit('Quitting Program')
-    elif x == "3":
-        list_json()
-    elif x == "d":
-        removeUser()
+	def inputData(x):
+	    if x == "1" or x == 'yes' or x == 'y':
+		addUser()
+	    elif x == "2" or x == 'no' or x == 'n':
+		exit('Quitting Program')
+	    elif x == "3":
+		list_json()
+	    elif x == "d":
+        	removeUser()
